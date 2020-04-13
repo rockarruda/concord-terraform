7c2e6dd7ad4970610456fd84db5cc659421fdaf9
Jason van Zyl <jason@vanzyl.ca>
2020-04-13 06:55:47 -0400

    Allowing for a longer waiting period for the RDS instance to be available

--- 0.12/aws/rds-postgres/tests/terraform-connect.sh
+++ 0.12/aws/rds-postgres/tests/terraform-connect.sh
@@ -2,7 +2,7 @@ rds_db_host="$(cat terraform-outputs.json | jq -r .rds_db_host.value)"
 rds_postgres_username="$(cat terraform-outputs.json | jq -r .rds_postgres_username.value)"
 rds_postgres_password="$(cat terraform.tfvars.json | jq -r .rds_postgres_password)"
 
-for i in {1..50}
+for i in {1..100}
 do
   PGPASSWORD=${rds_postgres_password} \
     pg_isready -h ${rds_db_host} -U ${rds_postgres_username} && break
