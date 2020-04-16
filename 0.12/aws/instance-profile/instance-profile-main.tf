5992d92de09f028d64a71be9583ce2fca3d35f5e
Jason van Zyl <jason@vanzyl.ca>
2020-04-16 15:48:17 -0400

    Allowing the specification of a role policy file

--- 0.12/aws/instance-profile/instance-profile-main.tf
+++ 0.12/aws/instance-profile/instance-profile-main.tf
@@ -9,7 +9,31 @@ resource "aws_iam_instance_profile" "main" {
   role = aws_iam_role.main.name
 }
 
+# ------------------------------------------------------------------------------
+# No policy file has been specified so we will will create a policy attachment
+# using the specified ARN
+# ------------------------------------------------------------------------------
+
 resource "aws_iam_role_policy_attachment" "main" {
+  count      = fileexists(var.instance_profile_role_policy) ? 0 : 1
   role       = aws_iam_role.main.name
   policy_arn = data.aws_iam_policy.selected.arn
+}
+
+# ------------------------------------------------------------------------------
+# A policy file has been specified. We read the json policy file into the
+# "aws_iam_policy" resource and then we attach it using the
+# "aws_iam_role_policy_attachment" resource.
+# ------------------------------------------------------------------------------
+
+resource "aws_iam_policy" "file" {
+  count  = fileexists(var.instance_profile_role_policy) ? 1 : 0
+  name   = aws_iam_role.main.name
+  policy = file(var.instance_profile_role_policy)
+}
+
+resource "aws_iam_role_policy_attachment" "file" {
+  count      = fileexists(var.instance_profile_role_policy) ? 1 : 0
+  role       = aws_iam_role.main.name
+  policy_arn = aws_iam_policy.file[0].arn
 }
--- 0.12/aws/instance-profile/instance-profile-variables.tf
+++ 0.12/aws/instance-profile/instance-profile-variables.tf
@@ -8,6 +8,12 @@ variable "instance_profile_role_arn" {
   description = "The role ARN to apply to this role"
 }
 
+variable "instance_profile_role_policy" {
+  type        = string
+  default     = "instance-profile-policy-role.json"
+  description = "The role policy file to apply to this role"
+}
+
 variable "instance_profile_assume_role_policy" {
   type = string
   default = "instance-profile-policy-assume-role.json"
